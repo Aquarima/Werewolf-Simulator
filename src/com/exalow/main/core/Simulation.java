eccf18382123d880d15213fc8eb4da07706b85eb
Exalow <53000164+Exalow@users.noreply.github.com>
2019-11-14 16:31:47 +0100

    Add files via upload

--- src/com/exalow/main/core/Simulation.java
+++ src/com/exalow/main/core/Simulation.java
@@ -1,20 +1,24 @@
 ﻿package com.exalow.main.core;
 
+import com.exalow.main.event.Event;
 import com.exalow.main.event.EventManager;
 import com.exalow.main.utils.Time;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Simulation {
 
-    private EventManager manager;
+    private EventManager manager = new EventManager();
     private Game game;
     private Time time;
     private int cycle;
 
     public Simulation() {
-        this.manager = new EventManager();
-        this.game new Game(manager);
-        this.time = new Time(game);
+        this.game = new Game(manager);
         game.load();
+        this.time = new Time(game);
     }
 
     public void start() {
@@ -27,7 +31,9 @@ public class Simulation {
     }
 
     private void stop() {
+        final DateFormat df = new SimpleDateFormat("hh:mm:ss");
         System.out.println("\nL'équipe " + game.getWinner() + " a gagnée !");
+        this.manager.addEvent(new Event(df.format(new Date()), game.getWinner()));
     }
 
 }
