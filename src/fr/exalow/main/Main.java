c9bf2e069476aa225aac79c753ba9595476e000a
Exalow <53000164+Exalow@users.noreply.github.com>
2019-11-25 09:51:20 +0100

    Add files via upload

--- src/fr/exalow/main/Main.java
+++ src/fr/exalow/main/Main.java
@@ -1,8 +1,10 @@
 package fr.exalow.main;
 
+import fr.exalow.main.core.Simulator;
+
 public class Main {
 
     public static void main(String[] args) {
-
+        new Simulator().start();
     }
 }
--- src/fr/exalow/main/utils/VoteHandler.java
+++ src/fr/exalow/main/utils/VoteHandler.java
@@ -1,4 +1,23 @@
 package fr.exalow.main.utils;
 
+import fr.exalow.main.entities.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class VoteHandler {
+
+    private List<Player> votes = new ArrayList<>();
+
+    public void addVote(Player player) {
+        this.votes.add(player);
+    }
+
+    public Player getResult() {
+        return null;
+    }
+
+    public void reset() {
+        this.votes.clear();
+    }
 }
